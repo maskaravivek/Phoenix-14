1e11bc657c8a5704eec8ed250f592af89f54dd1b
Vivek Maskara <maskaravivek@hotmail.com>
2014-03-04 03:15:18 +0530

    the final app
    the app is complete and is ready to be published

---
+++ Phoenix 14/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Phoenix_14.Resources;
+
+namespace Phoenix_14
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
